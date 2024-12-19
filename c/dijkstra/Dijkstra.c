666c2412d9faee5829205c20a69991f1ff9df28c
Oskar Sammet <oskar.sammet@st.oth-regensburg.de>
2024-12-19 12:59:49 +0100

    save

---
+++ c/dijkstra/Dijkstra.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct {
+    char* name;
+    int distance;
+} Node;
+
+void dijkstra();
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
